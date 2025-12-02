bf0787fa38f2a7d79c6d696ce81885aca9165e04
Valerii Hud <support@valerii-hud.com>
2025-12-02 20:15:50 +0100

    middleware: created protect routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { clerkMiddleware } from '@clerk/express';
+
 import userRoutes from './routes/user.route.js';
 import adminRoutes from './routes/admin.route.js';
 import authRoutes from './routes/auth.route.js';
 import songRoutes from './routes/song.route.js';
 import albumRoutes from './routes/album.route.js';
 import statRoutes from './routes/stat.route.js';
+
 import { connectDB } from './lib/db.js';
 dotenv.config({});
 
@@ -13,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(clerkMiddleware());
+
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/auth', authRoutes);
